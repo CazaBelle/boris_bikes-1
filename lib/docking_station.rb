76f062ffa00f84ccc7b686dfe1adc2a882fe4e8f
matharotheelf <mrthomasjacoblawrence@hotmail.co.uk>
2018-11-29 14:47:18 +0000

    excercise 11

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,6 +1,13 @@
 require_relative "./bike"
 class DockingStation
+  attr_reader :bike
+
   def release_bike
     return Bike.new
   end
+  def dock(bike)
+    @bike = bike
+  end
+
+
 end
--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -4,4 +4,7 @@ describe DockingStation do
   bike = DockingStation.new.release_bike
   it {expect(bike).to respond_to :is_working}
   it {expect(bike.is_working).to eq true}
+  docking_station = DockingStation.new
+  it {expect(docking_station.dock(bike)).to eq bike}
+  it {expect(docking_station.bike).to eq bike}
 end
